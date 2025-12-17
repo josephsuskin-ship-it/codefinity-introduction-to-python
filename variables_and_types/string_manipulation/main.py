94e812953f82fbe91caccdafdd15ac70b2732a3b
josephsuskin-ship-it <>
2025-12-17 20:40:37 +0000

    String Manipulation

--- variables_and_types/string_manipulation/main.py
+++ variables_and_types/string_manipulation/main.py
@@ -3,5 +3,14 @@ dairy1 = grocery_items[0:4]
 dairy2 = grocery_items[5:11]
 bakery1 = grocery_items[12:17]
 
-message = f"we have {dairy1}, {dairy2} and {bakery1} in aisle 5"
+message = ("We have dairy and bakery items: "
+           + dairy1 
+           + ","
+           + " "
+           + dairy2 
+           + ", and"
+           + " "
+           + bakery1 
+           + " in aisle 5"
+          )
 print(message)
